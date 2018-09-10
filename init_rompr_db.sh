426636e0f4368ce89c0ec3f49f295b49ecc72aba
tom roth <t.roth@moebel-dvorak.de>
2018-09-10 22:53:34 +0200

    separate apache and mysql container

--- init_rompr_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-mysql -uroot -e "CREATE USER 'rompr'@'localhost' IDENTIFIED BY 'romprdbpass'"
-mysql -uroot -e "CREATE DATABASE romprdb"
-mysql -uroot -e "GRANT ALL ON romprdb.* TO 'rompr'@'localhost'"
-
-
-echo "=> Done!"
